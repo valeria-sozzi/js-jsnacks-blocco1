17f8d746e025598c47be492a00e1cdad5ce540e6
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-05-09 10:27:38 +0200

    add snack-8 to modify and snack-9 completed

---
+++ snack-9/main.js
@@ -0,0 +1,17 @@
+//calcola la somma e la media dei primi 10 numeri
+
+//creo le costanti somma e media 
+let somma=0;
+let media=0;
+
+//creo il ciclo per prendere tutti i numeri
+for (let i=0; i<= 10; i++){
+    somma=somma+i
+
+    media=somma/10
+}
+
+console.log(somma);
+console.log(media);
+
+
